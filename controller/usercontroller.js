1905a7683a9f2836065a9eb5d1a3ed88bcc9685c
vickyyede28 <vicky20@navgurukul.org>
2022-08-24 17:41:23 +0530

    crud operation with Hapi.js with HashPassword

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,6 +1,7 @@
 const db = require("../mongodb/db")
 const {ObjectId} = require("mongodb");
-const { date } = require("joi");
+const bcrypt = require("bcrypt")
+const jwt  =require("jsonwebtoken")
 
 const createcompany  = async(req,h) =>{
     try {
@@ -39,9 +40,42 @@ const deletecompany = async(request,h)=>{
     }
 }
 
+const createuser = async(request,h)=>{
+    try {
+        const {name, email, password, confirmpassword} = request.payload
+        if(password!==confirmpassword){
+            return h.response({error:"Password and ConfirmPassword is not Valid!"})
+        } else {
+            const user = await db.get().collection("users").findOne({email});
+            if(user) return h.response({error:"User Already exists!"})
+            const hashPassword = await bcrypt.hash(password,10);
+            await db.get().collection("users").insertOne({name,email,password:hashPassword});
+            return h.response({success:"User Created Successfully!"})
+        }
+    } catch (error) {
+        console.log({error:error.message});
+    }
+}
+
+const loginuser =async(request,h)=>{
+    try {
+        const {email,password} = request.payload;
+        const user = await db.get().collection("users").findOne({email});
+        if(!user) return h.response({error:"users is not register"})
+        const data  = await bcrypt.compare(password,user.password);
+        if(!data) return h.response({error:"Credential is Invalid"})
+        return h.response({success:"User Login Successfully!!!"})
+
+    } catch (error) {
+        console.log({error:error.message});
+    }
+}
+
 module.exports = {
     createcompany,
     findcompany,
     updatecompany,
-    deletecompany
+    deletecompany,
+    createuser,
+    loginuser
 }
--- router/router.js
+++ router/router.js
@@ -57,6 +57,40 @@ const routes = [
                 })
             }
         }
+    },
+    {
+        method:"post",
+        path:"/register",
+        handler:companycontroller.createuser,
+        options:{
+            validate:{
+                payload:Joi.object({
+                    name:Joi.string().required(),
+                    email:Joi.string().required().email(),
+                    password:Joi.string().required(),
+                    confirmpassword:Joi.string().required()
+                }),
+                failAction: (request, h, error) => {
+                    return h.response({ message: error.details[0].message.replace(/['"]+/g, '') }).code(400).takeover();
+                }
+            }
+        }
+    },
+    {
+        method:"post",
+        path:"/login",
+        handler:companycontroller.loginuser,
+        options:{
+            validate:{
+                payload:Joi.object({
+                    email:Joi.string().email(),
+                    password:Joi.string()
+                }),
+                failAction: (request, h, error) => {
+                    return h.response({ message: error.details[0].message.replace(/['"]+/g, '') }).code(400).takeover();
+                }
+            }
+        }
     }
 
 ]
